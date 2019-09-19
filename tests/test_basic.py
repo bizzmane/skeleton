8d52c6077d5e5429a5bc9039ee801a8b57d7fb93
bizzmane <Hendrik.Bitzmann@gmail.com>
2019-09-19 20:43:08 +0200

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,15 +2,7 @@
 
 from .context import sample
 
-import unittest
+import pytest
 
-
-class BasicTestSuite(unittest.TestCase):
-    """Basic test cases."""
-
-    def test_absolute_truth_and_meaning(self):
-        assert True
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_absolute_truth_and_meaning(self):
+    assert True
